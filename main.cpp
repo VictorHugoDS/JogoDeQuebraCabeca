601084dfbf508de50857d994cff4b7dbf6f4eade
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2022-02-04 20:27:06 -0400

    Fix: remover comntários  desnecessários

--- main.cpp
+++ main.cpp
@@ -67,18 +67,6 @@ int main(int argc, char const *argv[])
     realiza_busca_profundidade();
   }
 
-
-
-  /*int numerosTabuleiro[8] = {1, 2, 3, 4, 5, 6, 7, 8};
-  Tabuleiro tabuleiro(2, numerosTabuleiro);
-
-  //int numerosObjetivo[8] = {1, 2, 3, 8, 4, 7, 6, 5};
-  //Tabuleiro objetivo(4, numerosObjetivo);
-
-  Arvore arvore(tabuleiro);
-  //Arvore arvore2(objetivo);
-  buscaEmLargura8Puzzle(&arvore);*/
-
   return 0;
 }
 
@@ -99,75 +87,3 @@ void realiza_busca_profundidade(){
     printf("\nNao e possivel de um quebra-cabeca chegar ao outro");
   }
 }
-// void buscaLargura(){
-//   int quantVertices;
-
-//   printf("Insira o numero de vertices: ");
-//   scanf("%d", &quantVertices);
-
-//   // int matrizAdj[quantVertices][quantVertices];
-
-//   vector<vector<int>> matrizAdj(quantVertices);
-//   for (int i = 0; i < quantVertices; i++)
-//   {
-//     matrizAdj[i].resize(quantVertices);
-//   }
-
-//   // Inicialização da matriz
-//   for (int vertice1 = 0; vertice1 < quantVertices; vertice1++)
-//   {
-//     for (int vertice2 = 0; vertice2 < quantVertices; vertice2++)
-//     {
-//       matrizAdj[vertice1][vertice2] = 0;
-//     }
-//   }
-
-//   int arestasEntreVerticesAtuais;
-
-//   // Preenchimento da matriz
-//   for (int vertice1 = 0; vertice1 < quantVertices; vertice1++)
-//   {
-//     arestasEntreVerticesAtuais = 0;
-//     for (int vertice2 = vertice1; vertice2 < quantVertices; vertice2++)
-//     {
-//       printf("Quantas arestas existem entre os vertices %d e %d: ", vertice1, vertice2);
-//       scanf("%d", &arestasEntreVerticesAtuais);
-
-//       matrizAdj[vertice1][vertice2] = arestasEntreVerticesAtuais;
-//       matrizAdj[vertice2][vertice1] = arestasEntreVerticesAtuais;
-//     }
-//   }
-
-//   printf("Matriz de adjacencia fornecida:\n");
-
-//   // Impressão da matriz
-//   for (int vertice1 = 0; vertice1 < quantVertices; vertice1++)
-//   {
-//     for (int vertice2 = 0; vertice2 < quantVertices; vertice2++)
-//     {
-//       printf("%d ", matrizAdj[vertice1][vertice2]);
-//     }
-//     printf("\n");
-//   }
-
-//   buscaEmLargura(quantVertices, matrizAdj, 0);
-  
-
-// /*
-//  int numeros[8] = {1,2,3,4,5,6,7,8};
-//  Tabuleiro tabuleiro(4, numeros);
-
-//  tabuleiro.printTabuleiro();
-
-//  std::cout << "=======================\n";
-//  tabuleiro.left();
-//  tabuleiro.printTabuleiro();
-//  std::cout << "=======================\n";
-//  tabuleiro.right();
-//  tabuleiro.printTabuleiro();
-//  std::cout << "=======================\n";
-//  tabuleiro.right();
-//  tabuleiro.printTabuleiro();
-
-//  */
-// }
